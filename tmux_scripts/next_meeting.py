70744cdab4b7cb25b6eedf85bd9de1a53760f775
Ivan Wang <ivan.wang@flyzipline.com>
2019-09-16 12:15:06 -0700

    Update meeting script to show recent past meetings but not more than 12 hours away

--- tmux_scripts/next_meeting.py
+++ tmux_scripts/next_meeting.py
@@ -3,28 +3,39 @@
 # Requires humanize (`pip3 install humanize`) and gcalcli.
 
 import subprocess
-from datetime import datetime
+from datetime import datetime, timedelta
 from humanize import naturaltime
+from typing import List
 
 CALENDAR = 'ivan.wang@flyzipline.com'
 GCALCLI_COMMAND = [
-    'gcalcli', 'agenda', '--tsv', '--nostarted', '--nodeclined', '--details', 'calendar']
+    'gcalcli', 'agenda', '--tsv', '--nodeclined', '--details', 'calendar']
+
+
+def get_recent_meeting(meetings: List[str], calendar: str) -> str:
+    for line in meetings:
+        if calendar not in line:
+            continue  # Ignore other calendars
+
+        next_meeting = line.split()
+        time = datetime.strptime('{} {}'.format(*next_meeting[:2]), '%Y-%m-%d %H:%M')
+        now = datetime.now()
+        diff = now - time
+        if diff > timedelta(minutes=30):
+            continue  # Ignore events from more than 30 minutes ago.
+
+        if diff < timedelta(hours=-12):
+            break  # Ignore meetings more than 12 hours away.
+
+        return '{}: {}'.format(naturaltime(diff), ' '.join(next_meeting[4:-1]))
+    return 'No meetings today!'
 
 
 if __name__ == '__main__':
-    print('hello')
     meetings = subprocess.run(
         GCALCLI_COMMAND,
         universal_newlines=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE).stdout
-    try:
-        next_meeting = next(
-            line for line in meetings.split('\n') if CALENDAR in line).split()
-        time = datetime.strptime('{} {}'.format(*next_meeting[:2]), '%Y-%m-%d %H:%M')
-        now = datetime.now()
-        diff = now - time
-        print('{}: {}'.format(naturaltime(diff), ' '.join(next_meeting[4:-1])))
-    except StopIteration:
-        print('No meetings scheduled!')
-print('hello')
+
+    print(get_recent_meeting(meetings.split('\n'), CALENDAR))
