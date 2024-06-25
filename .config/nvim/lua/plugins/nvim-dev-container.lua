7af7fd95a18885206cf0f73318c9738d138dc9a7
Ivan Wang <ivan.wang@flyzipline.com>
2024-06-24 22:56:53 -0700

    Run nearest test with python

--- .config/nvim/lua/plugins/nvim-dev-container.lua
+++ .config/nvim/lua/plugins/nvim-dev-container.lua
@@ -1,29 +1,53 @@
-local function GetGitRoot()
-  local current_dir = vim.fn.expand('%:p:h')
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.shellescape(current_dir) .. ' rev-parse --show-toplevel')[1]
-  return git_root
-end
+local utils = require("utils")
 
-local function GetFileRelativeToRoot()
+local function get_file_relative_to_root()
   local current_file = vim.fn.expand('%:p')
-  local git_root = GetGitRoot()
+  local git_root = utils.get_git_root()
   if git_root == '' then
     return current_file
   end
   return current_file:gsub(git_root, '')
 end
 
-local function runPytest()
+local function nearest_test()
+  local ts_utils = require("nvim-treesitter.ts_utils")
+  local current_node = ts_utils.get_node_at_cursor()
+  if not current_node then
+    return ""
+  end
+  --@type ts_utils.TSNode?
+  local expr = current_node
+  while expr do
+    if expr:type() == 'function_definition' then
+      break
+    end
+    expr = expr:parent()
+  end
+  if not expr then
+    return ""
+  end
+  return (vim.treesitter.query.get_node_text(expr:child(1)))[1]
+end
+
+local function run_pytest(nearest)
   -- Find the "service" name from the nearest devcontainer.json file
   local devcontainer = require('devcontainer.config_file.parse').parse_nearest_devcontainer_config()
   -- Find the current file relative to the root of the git repository
-  local current_file = GetFileRelativeToRoot()
+  local current_file = get_file_relative_to_root()
+
+  local args = {}
+  if nearest then
+    local nearest_test_name = nearest_test()
+    if nearest_test_name ~= "" then
+      args = { "-k " .. nearest_test_name }
+    end
+  end
 
   require('devcontainer.container').exec(
   -- TODO: Add support for customizing which container to run
     devcontainer and devcontainer.service or 'service',
     {
-      command = { 'pytest', current_file },
+      command = { 'pytest', current_file, unpack(args) },
       tty = true,
     }
   )
@@ -48,9 +72,8 @@ return {
       })
     end,
     keys = {
-      {
-        '<leader>tf', runPytest, desc = "Test file", ft = { 'python' }
-      }
+      { '<leader>tf', run_pytest,                      desc = "Test file",    ft = { 'python' } },
+      { '<leader>tn', function() run_pytest(true) end, desc = "Test nearest", ft = { 'python' } }
     }
   }
 }
