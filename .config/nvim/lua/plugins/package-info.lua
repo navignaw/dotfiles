6b8b63190cdf4b78b11d366b77fcf6ec0ec98fb2
Ivan Wang <ivanw100@gmail.com>
2024-06-28 14:43:52 -0700

    Add package-info vim package

---
+++ .config/nvim/lua/plugins/package-info.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "vuki656/package-info.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("package-info").setup({
+        colors = {
+          up_to_date = "#3c4048", -- Text color for up to date dependency virtual text
+          outdated = "#d19a66",   -- Text color for outdated dependency virtual text
+        },
+      })
+
+      wk = require("which-key")
+      wk.register({
+        ["<leader>pi"] = {
+          name = "Package Info",
+          t = {
+            function()
+              require("package-info").toggle()
+            end,
+            "Toggle package info",
+          },
+          u = {
+            function()
+              require("package-info").update()
+            end,
+            "Update package",
+          },
+          d = {
+            function()
+              require("package-info").delete()
+            end,
+            "Delete package",
+          },
+          i = {
+            function()
+              require("package-info").install()
+            end,
+            "Install package",
+          },
+          v = {
+            function()
+              require("package-info").change_version()
+            end,
+            "Change package version",
+          },
+        },
+      })
+    end,
+  },
+}
