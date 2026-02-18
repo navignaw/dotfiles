45baeb4396fc5cb01f41f8f83237f783a49bdbeb
Ivan Wang <ivan.wang@flyzipline.com>
2026-02-18 17:47:17 -0500

    Support tmux navigator in sidekick

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -3,17 +3,32 @@
 return {
   {
     "alexghergh/nvim-tmux-navigation",
-    config = function()
-      local navigation = require("nvim-tmux-navigation")
-
-      navigation.setup({})
-
-      vim.keymap.set("n", "<M-h>", navigation.NvimTmuxNavigateLeft, { desc = "Navigate left (tmux)" })
-      vim.keymap.set("n", "<M-j>", navigation.NvimTmuxNavigateDown, { desc = "Navigate down (tmux)" })
-      vim.keymap.set("n", "<M-k>", navigation.NvimTmuxNavigateUp, { desc = "Navigate up (tmux)" })
-      vim.keymap.set("n", "<M-l>", navigation.NvimTmuxNavigateRight, { desc = "Navigate right (tmux)" })
-      vim.keymap.set("n", "<M-\\>", navigation.NvimTmuxNavigateLastActive, { desc = "Navigate last active (tmux)" })
-      vim.keymap.set("n", "<M-Space>", navigation.NvimTmuxNavigateNext, { desc = "Navigate next (tmux)" })
-    end,
+    keys = {
+      {
+        "<M-h>",
+        "<CMD>lua require('nvim-tmux-navigation').NvimTmuxNavigateLeft()<CR>",
+        desc = "Tmux Left",
+        mode = { "n", "v", "i", "t" },
+      },
+      {
+        "<M-j>",
+        "<CMD>lua require('nvim-tmux-navigation').NvimTmuxNavigateDown()<CR>",
+        desc = "Tmux Down",
+        mode = { "n", "v", "i", "t" },
+      },
+      {
+        "<M-k>",
+        "<CMD>lua require('nvim-tmux-navigation').NvimTmuxNavigateUp()<CR>",
+        desc = "Tmux Up",
+        mode = { "n", "v", "i", "t" },
+      },
+      {
+        "<M-l>",
+        "<CMD>lua require('nvim-tmux-navigation').NvimTmuxNavigateRight()<CR>",
+        desc = "Tmux Right",
+        mode = { "n", "v", "i", "t" },
+      },
+    },
+    config = true,
   },
 }
